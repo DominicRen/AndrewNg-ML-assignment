d14dd43464e8514cecbdfc5fa7600a7f91008ae2
DominicRen <ren_9510@163.com>
2019-04-08 23:27:58 +0800

    reg

--- ex2/ex2_reg.py
+++ ex2/ex2_reg.py
@@ -48,5 +48,65 @@ lmd = 1
 # Compute and display initial cost and gradient for regularized logistic regression
 cost, grad = cfr.cost_function_reg(initial_theta, X, y, lmd)
 
+np.set_printoptions(formatter={'float': '{: 0.4f}\n'.format})
+print('Cost at initial theta (zeros): {}'.format(cost))
+print('Expected cost (approx): 0.693')
+print('Gradient at initial theta (zeros) - first five values only: \n{}'.format(grad[0:5]))
+print('Expected gradients (approx) - first five values only: \n 0.0085\n 0.0188\n 0.0001\n 0.0503\n 0.0115')
 
+input('Program paused. Press ENTER to continue')
+
+# Compute and display cost and gradient with non-zero theta
+test_theta = np.ones(X.shape[1])
+
+cost, grad = cfr.cost_function_reg(test_theta, X, y, lmd)
+
+print('Cost at test theta: {}'.format(cost))
+print('Expected cost (approx): 2.13')
+print('Gradient at test theta - first five values only: \n{}'.format(grad[0:5]))
+print('Expected gradients (approx) - first five values only: \n 0.3460\n 0.0851\n 0.1185\n 0.1506\n 0.0159')
+
+input('Program paused. Press ENTER to continue')
+
+# ===================== Part 2: Regularization and Accuracies =====================
+# Optional Exercise:
+# In this part, you will get to try different values of lambda and
+# see how regularization affects the decision boundary
+#
+# Try the following values of lambda (0, 1, 10, 100).
+#
+# How does the decision boundary change when you vary lambda? How does
+# the training set accuracy vary?
+#
+
+# Initializa fitting parameters
+initial_theta = np.zeros(X.shape[1])
+
+# Set regularization parameter lambda to 1 (you should vary this)
+lmd = 1
+
+# Optimize
+def cost_func(t):
+    return cfr.cost_function_reg(t, X, y, lmd)[0]
+
+def grad_func(t):
+    return cfr.cost_function_reg(t, X, y, lmd)[1]
+
+theta, cost, *unused = opt.fmin_bfgs(f=cost_func, fprime=grad_func, x0=initial_theta, maxiter=400, full_output=True, disp=False)
+
+# Plot boundary
+print('Plotting decision boundary ...')
+pdb.plot_decision_boundary(theta, X, y)
+plt.title('lambda = {}'.format(lmd))
+
+plt.xlabel('Microchip Test 1')
+plt.ylabel('Microchip Test 2')
+
+# Compute accuracy on our training set
+p = predict.predict(theta, X)
+
+print('Train Accuracy: {:0.4f}'.format(np.mean(y == p) * 100))
+print('Expected accuracy (with lambda = 1): 83.1 (approx)')
+
+input('ex2_reg Finished. Press ENTER to exit')
 
