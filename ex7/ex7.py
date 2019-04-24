344c2a139e94464d3cbaa004d1ffd30b9e92784f
DominicRen <ren_9510@163.com>
2019-04-24 16:57:48 +0800

    ex7

---
+++ ex7/ex7.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as scio
+from skimage import io
+from skimage import img_as_float
+# import runkMeans as km
+# import findClosestCentroids as fc
+# import computeCentroids as cc
+# import kMeansInitCentroids as kmic
+
+
+plt.ion()
+np.set_printoptions(formatter={'float': '{: 0.6f}'.format})
+
+
+
+
+
+
